1af987a2ee7d8cc026beb803de766b2d43c3e198
Robert Neher <robert.neher@bartscher-hasenaecker.de>
2020-02-06 12:09:46 +0100

    Backup todays work on reporting

--- KaffeeKraenzle.py
+++ KaffeeKraenzle.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import locale
 import pandas
 from pandas import DataFrame as df
 # import time
@@ -7,10 +8,17 @@ def main():
     KK_REVENUES = '2019_org.csv'
     # KK_REVENUES = 'Revenue{}'.format((datetime.datetime.now()).year)
 
-    revenues = pandas.read_csv(KK_REVENUES, sep=',', thousands='.', parse_dates=[0], dayfirst=True)
-    extended_data = extend_data(revenues)
-    print(revenues[:20])
-    
+    revenues = prepare_data(KK_REVENUES)
+    data = extend_data(revenues)
+    reporting(KK_REVENUES, data)
+
+def prepare_data(csv_file):
+    data = pandas.read_csv(csv_file, sep=',', thousands='.', parse_dates=[0], dayfirst=True)
+
+    # normalize dates in Date column
+    data.Date = pandas.to_datetime(data.Date)
+    return data
+
 def extend_data(data):
     weekNo_col = []
     weekDay_col = []
@@ -24,6 +32,24 @@ def extend_data(data):
     data['WeekDay'] = weekDay_col
 
     return data
+
+def reporting(dataset, data):
+    locale.setlocale(locale.LC_ALL, 'de_DE')
+    SEP_LENGTH = 40
+    print('File processed: ', dataset)
+    print('Total amount of data:', data['Date'].count())
+    print('='*SEP_LENGTH)
+    print('Total revenue: ', locale.currency(data['Amount'].sum(), grouping=True))
+    print('='*SEP_LENGTH)
+    print('Revenue per month:')
+    print(data.groupby(pandas.Grouper(key='Date', freq='1M')).sum())
+    print('='*SEP_LENGTH)
+    # print('='*SEP_LENGTH)
+    # print('Revenue per week:')
+    # print(data['WeekNo'].value_counts())
+    # print('='*20)
+
+    return
 
 if __name__ == '__main__':
     main()
