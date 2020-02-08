57a2d18bd044ded4cbf2c333de1082bfffd06917
rneher <robert.neher@bartscher-hasenaecker.de>
2020-02-08 14:36:15 +0100

    Full working reporting on text basis

--- KaffeeKraenzle.py
+++ KaffeeKraenzle.py
@@ -1,85 +1,85 @@
-# from __future__ import print_function
+import calendar
 from datetime import datetime
+import dateutil
 import json
 import locale
+from locale import atof
 import pandas
-from pandas import DataFrame as df
+from pandas import DataFrame
 import pickle
 import os.path
 import gspread
+from pprint import pprint
 from oauth2client.service_account import ServiceAccountCredentials
 # from googleapiclient.discovery import build
 # from google_auth_oauthlib.flow import InstalledAppFlow
 # from google.auth.transport.requests import Request
 
 def main():
-    KK_REVENUES = 'Revenues.csv'
-    # KK_REVENUES = 'Revenue{}'.format((datetime.datetime.now()).year)
+    GOOGLE_SHEET_NAME = 'Revenues'
 
-    revenues = openGoogleDoc('Revenues')
+    revenues = openGoogleDoc(GOOGLE_SHEET_NAME)
     data = extend_data(revenues)
-    reporting(KK_REVENUES, data)
-
-def prepare_data(csv_file):
-    data = pandas.read_csv(csv_file, sep=',', thousands='.', parse_dates=[0], dayfirst=True)
-
-    # normalize dates in Date column
-    data.Date = pandas.to_datetime(data.Date)
-    return data
+    reporting(data)
+    exit(0)
 
 def openGoogleDoc(docName):
     SCOPES = ['https://spreadsheets.google.com/feeds',
               'https://www.googleapis.com/auth/spreadsheets',
               'https://www.googleapis.com/auth/drive.file',
               'https://www.googleapis.com/auth/drive']
-    DOCUMENT_ID = '11dGMkS97OFD53Lcd_T-zDAxt29EjWKOte-a2qc3MntA'
-    SPREADSHEET = 'Revenues'
 
     credentials = ServiceAccountCredentials.from_json_keyfile_name("KaffeeKraenzle.json", SCOPES)
 
     client = gspread.authorize(credentials)
-    sheet = client.open(SPREADSHEET).sheet1  # Open the spreadhseet
-    data = sheet.get_all_records()  # Get a list of all records
-
-    return pd.DataFrame(data,mindex=data[:,0])
-
-
+    sheet = client.open(docName).sheet1  # Open the spreadhseet
+    df = pandas.DataFrame(sheet.get_all_records())
+    df.set_index('Date')
+    df['Date'] = df['Date'].apply(dateutil.parser.parse, dayfirst=True)
+    df['Amount'] = pandas.to_numeric(df['Amount'], errors='coerce')
+    
+    return df
+    
 def extend_data(data):
+    locale.setlocale(category=locale.LC_ALL, locale="German")
     weekDay_col = []
 
     for i, row in data.iterrows():
-        dt = row.Date
-        weekDay_col.append(dt.weekday())
+        weekDay_col.append(row.Date.weekday())
 
     data['WeekDay'] = weekDay_col
-
+    
     return data
 
-def reporting(dataset, data):
-    locale.setlocale(locale.LC_ALL, 'de_DE')
-    SEP_LENGTH = 40
-    print('File processed: ', dataset)
+def reporting(data):
+    SEP_LENGTH = 33
+    locale.setlocale(category=locale.LC_ALL, locale="German")
+
     print('Total amount of data:', data['Date'].count())
-    print('='*SEP_LENGTH)
-    print('Total revenue: ', locale.currency(data['Amount'].sum(), grouping=True))
-    print('='*SEP_LENGTH)
     
-    print('Revenue per year:')
-    records = data.groupby(pandas.Grouper(key='Date', freq='1Y')).sum()
-    
-    for i, row in records.iterrows():
-        print('{}\t{}'.format(i.year, locale.currency(row[0], grouping=True)))
+    yearData = data.groupby(pandas.Grouper(key='Date', freq='1Y')).sum()
 
-    print('='*SEP_LENGTH)
-    print('Revenue per month:')
-    records = data.groupby(pandas.Grouper(key='Date', freq='1M')).sum()
-    
-    for i, row in records.iterrows():
-        print('{:2d} {:4d}:{:>13}'.format(i.month, i.year, locale.currency(row[0], grouping=True)))
+    for year, row in yearData.iterrows():
+        print('='*SEP_LENGTH)
+        print('Year: {:4d} - {:>12s}'.format(year.year, locale.currency(row[0], grouping=True)))
+        print('-'*SEP_LENGTH)
 
-    print('='*SEP_LENGTH)
-    # print('Revenue per week:')
-    # print(data.groupby(pandas.Grouper(key='Date', freq='1W')).sum())
+        monthData = data.groupby(pandas.Grouper(key='Date', freq='1M')).sum()
+
+        for month, row in monthData.iterrows():
+            if month.year == year.year:
+                print('{:>12} {:4d}:{:>15}'.format(calendar.month_name[month.month], month.year, locale.currency(row[0], grouping=True)))
+    
+                print('-'*SEP_LENGTH)
+                print('Per week: {:2d}'.format(datetime.date(month).isocalendar()[1]))
+                print('-'*SEP_LENGTH)
+                weekData = data.groupby(pandas.Grouper(key='Date', freq='1W')).sum()
+
+                for week, row in weekData.iterrows():
+                    if (week.year == month.year) & (week.month == month.month):
+                        print('{:2} {:4d}:{:>15}'.format(datetime.date(week).isocalendar()[1], week.year, locale.currency(row[0], grouping=True)))
+
+                print('-'*SEP_LENGTH)
 
     return
 
